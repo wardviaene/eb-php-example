f4091eac06ff84b671026dda035d46b7ce55f701
Edward Viaene <ward@in4it.io>
2016-05-27 08:30:23 +0200

    php eb example

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php 
+echo '<p>Hello world!</p>'
+
+$mysqli = new mysqli(getenv('RDS_HOSTNAME'), getenv('RDS_USERNAME'), getenv('RDS_PASSWORD'), getenv('RDS_DATABASE'));
+if ($mysqli->connect_errno) {
+      echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
+echo $mysqli->host_info . "\n";
+
+?>
